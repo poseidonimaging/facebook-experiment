5b9baa1aafb87e3e61ac11a5c3d8cd76c819bd01
Keith Gable <ziggy@ignition-project.com>
2012-11-12 23:14:50 -0600

    Get tags from the status/checkin/etc.

--- js/application.js
+++ js/application.js
@@ -1,12 +1,15 @@
+// Global variables because I'm lazy.
+var fb_current_user_id;
+
 // jQuery event handlers
 $(document).ready(function () {
 	$("#fb-login").click(function () {
 		FB.login(function(response) {
 			if (response.authResponse) {
-				console.log('Welcome!  Fetching your information.... ');
+				fb_current_user_id = response.authResponse.userID;
 
 				$("#welcome").fadeOut("slow");
-				get_locations_from_url("/me/locations?fields=place,created_time,tags");
+				get_locations_from_url("/me/locations?fields=place,created_time,tags,from");
 			} else {
 				console.log('User cancelled login or did not fully authorize.');
 			}
@@ -32,7 +35,15 @@ function get_locations_from_url(url) {
 
 			var $place = $("#place_template").clone().find("li").first();
 
-			console.log($place);
+			// Make sure tags always includes the from and the tags, if they
+			// are available.
+			var tags = [];
+			if (result.data[i].tags && result.data[i].tags.data) {
+				tags = tags.concat(result.data[i].tags.data);
+			}
+			if (result.data[i].from) {
+				tags.push(result.data[i].from);
+			}
 
 			$place.attr("id", "place_" + result.data[i].place.id);
 			$place.attr("data-facebook-id", result.data[i].place.id); // Not using .data() because we need to be able to use a selector to find it.
@@ -43,6 +54,22 @@ function get_locations_from_url(url) {
 				.addClass("timeago")
 				.attr("datetime", result.data[i].created_time)
 				.text((new Date(result.data[i].created_time)).toString());
+
+			// Hide the been with list.
+			$place.find(".fb-been-with").hide();
+
+			if (tags.length > 0) {
+				for (var t = 0; t < tags.length; t++) {
+					// Don't add yourself.
+					if (tags[t].id != fb_current_user_id) {
+						var $person = $("<li>" + tags[t].name + "</li>");
+						$place.find(".fb-been-with ul").append($person);
+
+						// Show the been with list.
+						$place.find(".fb-been-with").show();
+					}
+				}
+			}
 
 			// so we can fade it in after adding it.
 			$place.hide();
