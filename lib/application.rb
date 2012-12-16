faf49780d923c8bc4824fde7407c2b70410ac840
Keith Gable <ziggy@ignition-project.com>
2012-12-16 14:29:19 -0600

    Add our Singly credentials, use our templates, and add a FB pass-thru

--- lib/application.rb
+++ lib/application.rb
@@ -1,3 +1,4 @@
+require "json"
 require "sinatra"
 require "omniauth-singly"
 require "httparty"
@@ -5,8 +6,8 @@ require "httparty"
 module MacroDeck
 	class SinglyController < Sinatra::Base
 		SINGLY_API_BASE = "https://api.singly.com"
-		SINGLY_ID = ""
-		SINGLY_SECRET = ""
+		SINGLY_ID = "a3745728d0ee7a409a711b18b81e4cb9"
+		SINGLY_SECRET = "9b03181e3cb40948842b14769fcc4f09"
 
 		enable :sessions
 		set :public_folder, File.join(File.expand_path(File.dirname(__FILE__)), "..", "public")
@@ -17,26 +18,43 @@ module MacroDeck
 		end
 
 		get "/" do
-			redirect "/index.html"
+			erb :"index.html", :layout => :"layout.html"
 		end
 
-		get "/facebook/?" do
-			endpoint = params.delete(:endpoint)
-			fields = params.delete(:fields)
+		get "/facebook/*" do
+			splat = params.delete("splat")
+			
+			# Delete params to not send to FB
+			params.delete("captures")
+			params.delete("access_token")
+
+			if splat && splat.length > 0
+				endpoint = splat[0]
+			else
+				endpoint = nil
+			end
 
 			if endpoint
+				query = { :access_token => session[:access_token] }.merge(params)
+
 				content_type :json
 				HTTParty.get("#{SINGLY_API_BASE}/proxy/facebook/#{endpoint}", {
-					:query => { :access_token => session[:access_token], :fields => fields }
-				}).parsed_response
+					:query => query
+				}).body
 			else
-				raise "Endpoint not specified"
+				raise "Endpoint not specified - splat=#{splat.inspect} params=#{params.inspect}"
 			end
 		end
 
 		get "/auth/singly/callback" do
 			auth = request.env["omniauth.auth"]
 			session[:access_token] = auth.credentials.token
+
+			# Get your Facebook ID.
+			session[:facebook_uid] = HTTParty.get("#{SINGLY_API_BASE}/proxy/facebook/me", {
+				:query => { :access_token => session[:access_token], :fields => "id" } 
+			}).parsed_response["id"]
+
 			redirect "/"
 		end
 
