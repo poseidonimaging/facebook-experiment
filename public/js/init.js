53b392c2497bbe9d8881ad4b5ba71c68196d1a25
Keith Gable <ziggy@ignition-project.com>
2012-12-16 14:29:47 -0600

    Remove Facebook stuff we don't use anymore.

--- public/js/init.js
+++ public/js/init.js
@@ -3,26 +3,3 @@
 // Google stuff
 google.load('visualization', '1.0', {'packages':['corechart']});
 google.load('maps', '3', {"other_params": "sensor=false&libraries=visualization"});
-
-// Facebook stuff
-window.fbAsyncInit = function() {
-	// init the FB JS SDK
-	FB.init({
-	  appId      : '167478190043137', // App ID from the App Dashboard
-	  channelUrl : '//fb.restlessnapkin.com/channel.html', // Channel File for x-domain communication
-	  status     : true, // check the login status upon init?
-	  cookie     : true, // set sessions cookies to allow your server to access the session?
-	  xfbml      : true  // parse XFBML tags on this page?
-	});
-
-	// Additional initialization code such as adding Event Listeners goes here
-};
-
-// Load the SDK's source Asynchronously
-(function(d, debug){
- var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
- if (d.getElementById(id)) {return;}
- js = d.createElement('script'); js.id = id; js.async = true;
- js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
- ref.parentNode.insertBefore(js, ref);
-}(document, /*debug*/ false));
