512815509072b767b912f067fa994ef114cf7495
Keith Gable <ziggy@ignition-project.com>
2012-12-17 22:51:14 -0600

    Send place/checkin data to SQS.

--- lib/application.rb
+++ lib/application.rb
@@ -2,12 +2,19 @@ require "json"
 require "sinatra"
 require "omniauth-singly"
 require "httparty"
+require "aws-sdk"
 
 module MacroDeck
 	class SinglyController < Sinatra::Base
 		SINGLY_API_BASE = "https://api.singly.com"
 		SINGLY_ID = "a3745728d0ee7a409a711b18b81e4cb9"
 		SINGLY_SECRET = "9b03181e3cb40948842b14769fcc4f09"
+
+		AWS_ACCESS_KEY = "AKIAINPARSP7PEW7I6DA"
+		AWS_SECRET_KEY = "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"
+
+		SQS_DATA_URL   = "https://sqs.us-east-1.amazonaws.com/766921168018/RestNap_OpenGraph_DataWithLocations"
+		SQS_PLACES_URL = "https://sqs.us-east-1.amazonaws.com/766921168018/RestNap_OpenGraph_Places"
 
 		enable :sessions
 		set :public_folder, File.join(File.expand_path(File.dirname(__FILE__)), "..", "public")
@@ -35,12 +42,44 @@ module MacroDeck
 			end
 
 			if endpoint
+				content_type :json
+
 				query = { :access_token => session[:access_token] }.merge(params)
 
-				content_type :json
-				HTTParty.get("#{SINGLY_API_BASE}/proxy/facebook/#{endpoint}", {
+				# Do the query.
+				resp = HTTParty.get("#{SINGLY_API_BASE}/proxy/facebook/#{endpoint}", {
 					:query => query
-				}).body
+				})
+
+				# Get the parsed response
+				parsed = resp.parsed_response
+
+				# See if we have data to send off.
+				if !parsed["data"].nil? && parsed["data"].length > 0
+					# Get a reference to the SQS queue
+					places_queue = AWS::SQS.new(:access_key_id => AWS_ACCESS_KEY, :secret_access_key => AWS_SECRET_KEY).queues[SQS_PLACES_URL]
+					data_queue = AWS::SQS.new(:access_key_id => AWS_ACCESS_KEY, :secret_access_key => AWS_SECRET_KEY).queues[SQS_DATA_URL]
+
+					# Loop over all the pieces of data.
+					parsed["data"].each do |item|
+						if item["place"]
+							# Send the place to the places queue
+							places_queue.send_message(item["place"].to_json)
+
+							# Make the place be a pointer to the place ID.
+							item["place"] = item["place"]["id"]
+
+							# Send the entire item to SQS.
+							data_queue.send_message(item.to_json)
+						else
+							# Send the entire item to SQS.
+							data_queue.send_message(item.to_json)
+						end
+					end
+				end
+
+				# No need to re-JSON-encode anything
+				return resp.body
 			else
 				raise "Endpoint not specified - splat=#{splat.inspect} params=#{params.inspect}"
 			end
