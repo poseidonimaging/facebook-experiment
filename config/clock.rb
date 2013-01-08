117717f4a684dabb19561e2bae4928556680a438
Keith Gable <ziggy@ignition-project.com>
2013-01-07 23:54:12 -0600

    The clockwork configuration file.

---
+++ config/clock.rb
@@ -0,0 +1,11 @@
+require "clockwork"
+require "rake"
+
+# Load the normal Rakefile.
+load File.join(File.dirname(__FILE__), "..", "Rakefile")
+
+module Clockwork
+	every(1.hour, "restnap:process:users") do
+		Rake::Task["restnap:process:users"].invoke
+	end
+end
