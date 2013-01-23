04b0318c46300971dd25c5fcbaef0d0f740c314f
Keith Gable <ziggy@ignition-project.com>
2013-01-22 22:05:49 -0600

    Run the places processing task every hour.

--- config/clock.rb
+++ config/clock.rb
@@ -5,6 +5,10 @@ require "rake"
 load File.join(File.dirname(__FILE__), "..", "Rakefile")
 
 module Clockwork
+	every(1.hour, "restnap:process:places") do
+		Rake::Task["restnap:process:places"].invoke
+	end
+
 	every(1.hour, "restnap:process:users") do
 		Rake::Task["restnap:process:users"].invoke
 	end
