cb2cc9cec2ed51a9ef637bacf0986231acb6c468
Keith Gable <ziggy@ignition-project.com>
2013-03-17 18:52:58 -0500

    Instead of handling tags here, pass them in as HTML5 data.

--- public/js/application.js
+++ public/js/application.js
@@ -254,36 +254,10 @@ function process_facebook_response(result) {
 						about: data.place.about,
 						phone: data.place.phone,
 						picture: !!(data.place.picture && !data.place.picture.data.is_silhouette),
-						cover_id: !!(data.place.cover && data.place.cover.cover_id) ? data.place.cover.cover_id : null
-					});
-
-					$(document).on("restnap:place:card_added", "#place_".concat(data.place.id), function() {
-						// Populate visited with list if we can.
-						if (tags.length > 0 && $place.find(".fb-visit-with-list").text() == "by yourself") {
-							var visit_with_list = [];
-
-							// Loop over the tags and add the people you've been with but not yourself.
-							for (var t = 0; t < tags.length; t++) {
-								// Don't add yourself.
-								if (tags[t].id != fb_current_user_id) {
-									visit_with_list.push(tags[t].name);
-
-									// Add to the list in the top-right.
-									var visit_count_person_html = $.mustache(templates.visit_count_person, {
-										place_id: data.place.id,
-										person_id: tags[t].id,
-										person_name: tags[t].name,
-										visit_count: 1,
-										hidden: true
-									});
-									$place.find(".fb-visit-counts").append(visit_count_person_html);
-								}
-							}
-
-							// Now use array_to_sentence to make it magic.
-							if (visit_with_list.length > 0) {
-								$place.find(".fb-visit-with-list").text("with " + array_to_sentence(visit_with_list));
-							}
+						cover_id: !!(data.place.cover && data.place.cover.cover_id) ? data.place.cover.cover_id : null,
+						location: data.place.location,
+						data: {
+							tags: tags
 						}
 					});
 				} else {
